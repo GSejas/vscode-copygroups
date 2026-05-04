142c7ae0da07e7a47930c57cf826e0f39005d885
Jorge <jsequeira03@gmail.com>
2026-05-03 18:54:59 -0600

    security: add Content-Security-Policy meta tag to webview

--- src/presentation/webview/CopyGroupsWebviewProvider.ts
+++ src/presentation/webview/CopyGroupsWebviewProvider.ts
@@ -332,6 +332,7 @@ export class CopyGroupsWebviewProvider implements vscode.WebviewViewProvider {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src ${webview.cspSource};">
     <title>Copy Groups</title>
     <link rel="stylesheet" href="${styleUri}">
 </head>
